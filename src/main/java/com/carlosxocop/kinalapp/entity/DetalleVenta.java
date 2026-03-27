a84a2b4a58af2029893f2903955d196e57d4ab57
cxocop-2025287 <cxocop-2025287@kinal.edu.gt>
2026-03-26 19:57:37 -0600

    feat: se agrega la entidad DetalleVenta

---
+++ src/main/java/com/carlosxocop/kinalapp/entity/DetalleVenta.java
@@ -0,0 +1,88 @@
+package com.carlosxocop.kinalapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "detalle_venta")
+public class DetalleVenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo_detalle;
+
+    @Column(nullable = false)
+    private int cantidad;
+
+    @Column(name = "precio_unitario", nullable = false)
+    private double precio;
+
+    @Column(nullable = false)
+    private double subtotal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "productos_codigo_producto", nullable = false)
+    private Producto producto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ventas_codigo_venta", nullable = false)
+    private Venta venta;
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(Long codigo_detalle, int cantidad, double precio, double subtotal, Producto producto, Venta venta) {
+        this.codigo_detalle = codigo_detalle;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.subtotal = subtotal;
+        this.producto = producto;
+        this.venta = venta;
+    }
+
+    public Long getCodigo_detalle() {
+        return codigo_detalle;
+    }
+
+    public void setCodigo_detalle(Long codigo_detalle) {
+        this.codigo_detalle = codigo_detalle;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+}
